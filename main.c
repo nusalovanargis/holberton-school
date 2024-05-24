7f559f9a0483ab4762baf6ea42325584c0d47a09
Nargıs <nargisnusalova@Nargs-MacBook-Pro.local>
2024-05-24 10:40:35 +0400

    print School

--- main.c
+++ main.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
-
 int main()
 {
   int i;
-  for (i = 0; i < 32; i++)
+  for (i = 0; i < 10; i++)
   {
-    printf("Hello School\n");
+    printf("School\n");
   }
   return 0;
 }
